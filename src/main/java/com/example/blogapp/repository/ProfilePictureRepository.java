99ea3a9e4da0ff1ed01c3fb9960e6dc02323bfae
M Sharique Siddiqui <157228213+m-sharique@users.noreply.github.com>
2024-07-21 16:35:38 +0530

    Coded Profile Picture Repository

--- src/main/java/com/example/blogapp/repository/ProfilePictureRepository.java
+++ src/main/java/com/example/blogapp/repository/ProfilePictureRepository.java
@@ -1,2 +1,10 @@
-package com.example.blogapp.repository;public class ProfilePictureRepository {
+package com.example.blogapp.repository;
+
+import com.example.blogapp.model.ProfilePicture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfilePictureRepository extends JpaRepository<ProfilePicture, Long> {
+    Optional<ProfilePicture> findByUserId(Long userId);
 }
